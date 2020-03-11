ad25da8ae4dc17802ee01c3a9bd99f64899208a5
Kevin Alvarez <kevin.alvarez@nearshorecode.com>
2020-03-11 19:50:16 -0400

    feat: get current game in container

--- treasure-hunter-wep-app/src/components/Game/GameContainer.js
+++ treasure-hunter-wep-app/src/components/Game/GameContainer.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getCurrentGame } from '../../services';
+
+const getGame = async () => {
+  try {
+    const response = await getCurrentGame();
+    console.info(response);
+  } catch (error) {
+    console.info(error);
+  }
+};
 
 export const GameContainer = () => {
+  useEffect(() => {
+    getGame();
+  });
   return <div>Holis</div>;
 };
