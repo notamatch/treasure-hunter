d42219316e6a0d24f31da7527568f084ba95ba02
Kevin Alvarez <kevin.alvarez@nearshorecode.com>
2020-03-11 19:49:59 -0400

    feat: config cors

--- treasure-hunter-api/src/index.js
+++ treasure-hunter-api/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { router } = require('./modules/router');
 
 const port = process.env.PORT || 3002;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1', router);
 
