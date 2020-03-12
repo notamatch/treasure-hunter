078e36a2bae25bcb93f4d06dc48ae47bf938f101
Kevin Alvarez <kevin.alvarez@nearshorecode.com>
2020-03-11 20:26:01 -0400

    feat: action to start game

--- treasure-hunter-wep-app/src/index.js
+++ treasure-hunter-wep-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { App } from './components/App';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
 import { reducers } from './reducers';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import 'rsuite/lib/styles/index.less';
+import 'rsuite/dist/styles/rsuite-default.css';
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- treasure-hunter-wep-app/src/reducers/game/actions.js
+++ treasure-hunter-wep-app/src/reducers/game/actions.js
@@ -1,6 +1,16 @@
+import { createGame } from '../../services/games';
 import { GAME_ACTIONS } from './constants';
 
 export const loadAction = (data) => ({
   type: GAME_ACTIONS.LOAD,
   payload: data
 });
+
+export const startGameAction = (dispatch) => async (player) => {
+  try {
+    const response = await createGame(player);
+    dispatch(loadAction(response.data));
+  } catch (error) {
+    console.info(error);
+  }
+};
