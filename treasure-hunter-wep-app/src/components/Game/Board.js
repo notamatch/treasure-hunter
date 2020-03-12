745a1367a78217b6eba9ff96b86225e7c71e5190
Kevin Alvarez <kevin.alvarez@nearshorecode.com>
2020-03-12 00:02:20 -0400

    feat: add position to turn

--- treasure-hunter-wep-app/src/components/Game/Board.js
+++ treasure-hunter-wep-app/src/components/Game/Board.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import { FlexboxGrid, Button } from 'rsuite';
+import { turnActions } from '../../reducers/turn';
 
-const Row = ({ row = [] }) => (
-  row.map((_, index) => <Button key={index}>?</Button>)
+const Row = ({ data, row, onAdd }) => (
+  data.map((_, column) => (
+    <Button
+      key={column}
+      onClick={() => onAdd(row, column)}>
+      ?
+    </Button>
+  ))
 );
 
+const selector = (state) => (state.turn);
+
+const getOnAdd = (dispatch, turn) => (row, column) => {
+  if (turn.length < 3) {
+    dispatch(turnActions.addAction(row, column))
+  }
+};
+
 export const Board = ({ board }) => {
+  const dispatch = useDispatch();
+  const turn = useSelector(selector, shallowEqual);
+  const onAdd = getOnAdd(dispatch, turn);
   return (
     <>
-      {board.map((row, index) => (
+      {board.map((data, index) => (
         <FlexboxGrid key={index}>
-          <Row row={row} />
+          <Row
+            data={data}
+            row={index}
+            onAdd={onAdd} />
         </FlexboxGrid>
       ))}
     </>
--- treasure-hunter-wep-app/src/reducers/turn/reducer.js
+++ treasure-hunter-wep-app/src/reducers/turn/reducer.js
@@ -4,7 +4,7 @@ const initialState = [];
 
 export const turn = (state = initialState, action) => {
   switch (action.type) {
-    case TURN_ACTIONS.LOAD:
+    case TURN_ACTIONS.ADD:
       return [...state, action.payload];
     case TURN_ACTIONS.CLEAN:
       return [...initialState];
