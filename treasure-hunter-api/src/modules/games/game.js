b96730b2bb958f782a30690666c6907a6a82dee5
Kevin Alvarez <kevin.alvarez@nearshorecode.com>
2020-03-12 02:38:05 -0400

    feat: count turns

--- treasure-hunter-api/src/modules/games/game.js
+++ treasure-hunter-api/src/modules/games/game.js
@@ -1,11 +1,13 @@
 const { createStaticBoard, getGameBoard } = require('./board');
-const { makeCopy } = require('./utils');
+const { makeCopy, visualizeBoard } = require('./utils');
 const { VALUES, TREASURE_QUANTITY } = require('./constants');
 
 let currentPlayer = '';
 let staticBoard = [];
 let gameBoard = [];
 let foundTreasures = 0;
+let numberOfTurns = 0;
+let top10List = [];
 
 const startGame = (data) => {
   const { player } = data;
@@ -13,6 +15,8 @@ const startGame = (data) => {
   gameBoard = getGameBoard();
   currentPlayer = player;
   foundTreasures = 0;
+  numberOfTurns = 0;
+  visualizeBoard(staticBoard);
   return {
     player,
     board: makeCopy(gameBoard)
@@ -23,8 +27,23 @@ const getCurrentGame = () => {
   return {
     player: currentPlayer,
     board: makeCopy(gameBoard),
-    win: foundTreasures === TREASURE_QUANTITY
+    win: foundTreasures === TREASURE_QUANTITY,
+    top10List
   };
+};
+
+const validateTreasure = (value) => {
+  if (value === VALUES.TREASURE) {
+    ++foundTreasures;
+  }
+  if (foundTreasures === TREASURE_QUANTITY) {
+    top10List.push({
+      player: currentPlayer,
+      score: numberOfTurns
+    });
+    top10List = top10List.sort((element1, element2) => element1.score > element2.score);
+    top10List = top10List.slice(0, 9);
+  }
 };
 
 const revealPositions = (positions) => {
@@ -32,15 +51,14 @@ const revealPositions = (positions) => {
     const { row, column } = position;
     const value = staticBoard[row][column];
     gameBoard[row][column] = value;
-    if (value === VALUES.TREASURE) {
-      ++foundTreasures;
-    }
+    validateTreasure(value);
     return value;
   });
 };
 
 const playTurn = (data) => {
   const { positions } = data;
+  ++numberOfTurns;
   const values = revealPositions(positions);
   return {
     values,
